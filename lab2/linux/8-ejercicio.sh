c25d8b240ac1f0377b2348a3f4acf325f8509b84
javiton2005 <javiton2005@gmail.com>
2025-02-03 11:28:17 +0100

    7-8 linux

--- lab2/linux/8-ejercicio.sh
+++ lab2/linux/8-ejercicio.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-read -p "¿Está seguro de que desea borrar el directorio MiDirectorio?
-[y/n] " confirm
+read -p "¿Está seguro de que desea borrar el directorio MiDirectorio? [y/n] " confirm
 if [ "$confirm" = "y" ]; then
-  rm -rf MiDirectorio
-  echo "MiDirectorio y su contenido han sido eliminados."
+  for file in ./MiDirectorio/*; do
+    read -p "Estas seguro que quiere eliminar el fichero $file? [y/n]" confirm 
+    if [ "$confirm" = "y" ]; then
+      echo "Se elimino el archivo: $file" >> archivos-eliminados.txt
+    fi 
+  done
+  #rm -rf MiDirectorio
+  #echo "MiDirectorio y su contenido han sido eliminados."
 else
   echo "Operación cancelada."
 fi
-
