0d64c8b60d070e6ad81b20636589782668e247ed
javiton2005 <javiton2005@gmail.com>
2025-02-06 17:56:42 +0100

    acabado los de linux

--- lab2/linux/8-ejercicio.sh
+++ lab2/linux/8-ejercicio.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 read -p "¿Está seguro de que desea borrar el directorio MiDirectorio? [y/n] " confirm
+Variable=0
 if [ "$confirm" = "y" ]; then
   for file in ./MiDirectorio/*; do
     read -p "Estas seguro que quiere eliminar el fichero $file? [y/n]" confirm 
     if [ "$confirm" = "y" ]; then
+      Variable++
+      rm $file
       echo "Se elimino el archivo: $file" >> archivos-eliminados.txt
     fi 
   done
-  #rm -rf MiDirectorio
-  #echo "MiDirectorio y su contenido han sido eliminados."
+  if [[ Variable!=0 ]]; then
+    rm -rf MiDirectorio
+    echo "MiDirectorio y su contenido han sido eliminados."
+  else
+    echo "MiDirectorio no se a borrado por que no quieres eliminar ningun fichero"
+  fi
+
 else
   echo "Operación cancelada."
 fi
--- lab2/linux/9-ejercicio.sh
+++ lab2/linux/9-ejercicio.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-sourceDir="/ruta/del/directorio"
+
+sourceDir="./MiDirectorio/"
+
 outputFile="combined.txt" > "$outputFile"
+
 for file in "$sourceDir"/*.txt; do
   echo "Combinando: $file"
   cat "$file" >> "$outputFile"
   echo "" >> "$outputFile" # Añade una línea en blanco entre los contenidos de los archivos
 done
+
+sort -u -o "combined.txt" "combined.txt"
+
 echo "Todos los archivos .txt han sido combinados en $outputFile."
