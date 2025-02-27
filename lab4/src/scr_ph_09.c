9ac573175fd877c4027d9daac91095c900e4d41a
Jaime Ordovás <150004573+Jaimeordovas@users.noreply.github.com>
2025-02-28 00:01:23 +0100

    Add files via upload

--- lab4/src/scr_ph_09.c
+++ lab4/src/scr_ph_09.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+
+#define MAX_LINEA 256
 
 // ./base/
 // └── sub1/
@@ -8,23 +12,28 @@
 
 
 int main() {
-    // Crear el directorio base
-    if (mkdir("./base", 0777) == -1) {
-        perror("Error al crear el directorio base");
-        return 1;
+    FILE *file = fopen("properties.txt", "r");
+    if(!file){
+	perror("Error al abrir archivo properties");
+	return 1;
+    } else {
+        printf("Archivo properties abierto correctamente");
     }
 
-    // Crear el primer subdirectorio dentro del directorio base
-    if (mkdir("./base/sub1", 0777) == -1) {
-        perror("Error al crear el primer subdirectorio");
-        return 1;
+    char line[MAX_LINEA]; //crea array para guardar lineas del archivo properties
+
+    while(fgets(line, sizeof(line), file)) { //guarda en line contenido de array
+
+        line[strcspn(line, "\n")] = 0; //remplaza \n con NULL
+
+	if (mkdir(line, 0777) == -1) {
+	    perror("Error al crear el directorio base");
+    	} else {
+	    printf("Directorio Creado: %s\n", line);
+	}
     }
 
-    // Crear un segundo subdirectorio dentro del primer subdirectorio
-    if (mkdir("./base/sub1/sub2", 0777) == -1) {
-        perror("Error al crear el segundo subdirectorio");
-        return 1;
-    }
+    fclose(file);
 
     printf("Estructura de directorios creada con éxito.\n");
 
