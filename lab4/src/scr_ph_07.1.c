865218e20e1bffea679b2ba47a0ca53afa7edb70
Javiton2005 <9303039@alumnos.ufv.es>
2025-02-27 20:21:56 +0100

    Cambiado la posicion del ejercicio 7 y 7.1

--- lab4/src/scr_ph_07.1.c
+++ lab4/src/scr_ph_07.1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <time.h>
 
 int main(int argc, char *argv[]) {
     int num1 = 0, num2 = 0; // Variables para almacenar los números
@@ -11,6 +12,12 @@ int main(int argc, char *argv[]) {
         {"cadena", required_argument, 0, 'c'},
         {"num1",   required_argument, 0, 'n'},
         {"num2",   required_argument, 0, 'm'},
+        {"version", no_argument,       0, 'v'},
+        {"date",    no_argument,       0, 'd'},
+        {"name",    no_argument,       0, 'N'},
+        {"last",    no_argument,       0, 'l'},
+        {"full",    no_argument,       0, 'f'},
+        {"birth",   no_argument,       0, 'b'},
         {0, 0, 0, 0}
     };
 
@@ -18,7 +25,7 @@ int main(int argc, char *argv[]) {
     int opt = 0;
 
     // Procesar las opciones pasadas
-    while ((opt = getopt_long(argc, argv, "c:n:m:", long_options, &long_index)) != -1) {
+    while ((opt = getopt_long(argc, argv, "c:n:m:v:d:N:l:f:b", long_options, &long_index)) != -1) {
         switch (opt) {
             case 'c':
                 cadena = optarg;
@@ -29,8 +36,26 @@ int main(int argc, char *argv[]) {
             case 'm':
                 num2 = atoi(optarg);
                 break;
+            case 'v':
+                printf("Versión 0.1.0 - Fase Alpha\n");
+                return 0;
+            case 'd':
+                printf("Fecha: Día de pierna no, te lo aseguro\n");
+                return 0;
+            case 'N':
+                printf("Nombre: Juan Diego\n");
+                return 0;
+            case 'l':
+                printf("Apellido: Panchón\n");
+                return 0;
+            case 'f':
+                printf("Nombre completo: Juan Diego Panchón\n");
+                return 0;
+            case 'b':
+                printf("Fecha de nacimiento: 32-13-2105\n");
+                return 0;
             default:
-                printf("Uso: %s --cadena <cadena> --num1 <numero1> --num2 <numero2>\n", argv[0]);
+                printf("Uso: %s --cadena <cadena> --num1 <numero1> --num2 <numero2> [opciones]\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
     }
