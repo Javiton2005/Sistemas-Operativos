657f7fce61976e59a551eae561b5b39159166013
Javiton2005 <9303039@alumnos.ufv.es>
2025-02-28 09:57:36 +0100

    Inicio banco

--- lab6/banco.c
+++ lab6/banco.c
@@ -1,8 +1,30 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <signal.h>
 #include <threads.h>
+#include <unistd.h>
+#include <string.h>
+int fd[2];
 
+void manejador() {
+  int dinero_prueba;
+  read(fd[0], &dinero_prueba, sizeof(dinero_prueba));
+  printf("Esto imprime algo");
+  if(dinero_prueba>10)
+    printf("Es una cantidad de millonarios->%d\n",dinero_prueba);
+  close(fd[0]);
+}
 int main(int argc, char *argv[]){
-  printf("Esto comienza\n");
+  int pid;
+  int dinero;
+  int fd[2];
+  pipe(fd);
+  close(fd[0]);
+  while (true) {
+    scanf("%d", &dinero);
+    write(fd[1], &dinero, sizeof(dinero));
+    signal(SIGUSR1, manejador);
+    close(fd[1]);
+  }
   return 1;
 }
