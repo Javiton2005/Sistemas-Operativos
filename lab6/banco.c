7d5b4fcd50a21a509f00bfa294e55f500ef33f8b
Javier <93844376+Javiton2005@users.noreply.github.com>
2025-03-26 13:19:20 +0100

    copia

--- lab6/banco.c
+++ lab6/banco.c
@@ -8,10 +8,10 @@
  *
 */
 
+
 #include "comun/comun.h"
 #include "usuarios/usuarios.h"
 #include "login/login.h"
-#include "monitor/monitor.h"
 #include <stdio.h>
 
 
@@ -21,50 +21,20 @@ int main(){
   char salir='a';
   InitGlobal();
 
-  int pipe_alerta[2]; // Descriptores de lectura y escritura del pipe
-
-  if (pipe(pipe_alerta) == -1) { // Crear el pipe
-    perror("Error en pipe");
-    exit(EXIT_FAILURE);
-  }
-
-  pid_t pid = fork(); // Crear un proceso hijo
-
-  if (pid < 0) { // Comprobar si ha habido error
-    perror("Error en fork");
-    exit(EXIT_FAILURE);
-  }
-
-  if (pid == 0) {
-    close(pipe_alerta[1]); // Cerrar el descriptor de escritura del pipe
-    monitor(pipe_alerta[0]); // Lanzar el proceso monitor
-    exit(0);
-  } else {
-    close(pipe_alerta[0]); // Cerrar el descriptor de lectura del pipe
-
-    while (salir!='*') {
-      system("clear");
-      listaUsuarios=CrearListaUsuarios(Config.archivo_cuentas);
-      if(listaUsuarios==NULL){
-        printf("Error en la creaccion de Lista de Usuarios");
-        exit(-1);
-      }
-      
-      char alerta[256];
-      ssize_t alerta_size = read(pipe_alerta[0], alerta, sizeof(alerta)-1);
-      if (alerta_size > 0) {
-        alerta[alerta_size] = '\0';
-        printf("ALERTA: %s\n", alerta);
-      } else {
-        perror("read");
-      }
-
-      printf("Para salir presiona *: ");
-      while (getchar() != '\n'); // Limpiamos el buffer de entrada
-      salir=getchar();
+  while (salir!='*') {
+    system("clear");
+    listaUsuarios=CrearListaUsuarios(Config.archivo_cuentas);
+    if(listaUsuarios==NULL){
+      printf("Error en la creaccion de Lista de Usuarios");
+      exit(-1);
     }
+    
+    //printf("nombre: %s\nContraseña %s",listaUsuarios[Estadisticas.usuarios]->nombre,listaUsuarios[Estadisticas.usuarios]->contrasena);
 
-    close(pipe_alerta[0]); // Cerrar el descriptor de escritura del pipe
+    login(listaUsuarios);
+    printf("Para salir presiona *: ");
+    while (getchar() != '\n'); // Limpiamos el buffer de entrada
+    salir=getchar();
   }
   
   return 1;
--- lab6/usuario.c
+++ lab6/usuario.c
@@ -2,9 +2,7 @@
 #include "funciones/funciones.h"
 
 int main(int argc, char *argv[]){
-  USER usuario = {argv[1],argv[2],argv[3],atoi(argv[4]),atoi(argv[5]),atoi(argv[6])};   
-  char *log=argv[7];
-
+  USER usuario = {argv[1],argv[2],argv[3],atoi(argv[4]),atoi(argv[5])};
   while (1) {  
     MenuOpciones(&usuario);
   }
