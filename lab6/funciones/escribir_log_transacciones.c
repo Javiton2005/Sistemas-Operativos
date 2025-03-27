dbd8f038993b57447be29824e833c4abecb0822e
Panch1027373 <jdpv645294509@gmail.com>
2025-03-27 09:36:17 +0100

    funciones CancelarTarjeta, EscribirLogTransacciones, MeterDinero, PagraTasa, Transaccion, PROVISIONAL

---
+++ lab6/funciones/escribir_log_transacciones.c
@@ -0,0 +1,15 @@
+#include "comun.h"
+
+void EscribirLogTrans(TRANSACCION trans){
+  char fecha[50], mensaje[100];
+
+  // Coje la fecha del sistema
+  strftime(fecha, sizeof(fecha), "%d-%m-%Y %H:%M:%S", trans.fecha);
+  snprintf(mensaje, sizeof(mensaje),"%s %s\n", fecha, trans.descripcion);
+  // Bloquea el sistema por medio de semaforos
+  sem_t *sem = sem_open("/sem_log_trans", O_CREAT, 0644, 1);
+  FILE *log = fopen("../ficheros/transacciones.log","a"); // Abre el fichero
+  fputs(trans.descripcion, log); // Añade la linea al final del fichero
+  fclose(log);
+  sem_post(sem);
+}
--- lab6/funciones/meter_dinero.c
+++ lab6/funciones/meter_dinero.c
@@ -1,5 +1,27 @@
 #include "funciones.h"
 
-void MeterDinero(int *idUser){
-  
+int MeterDinero(){
+    float c;
+    printf("Cantidad a ingresar: \n");
+    scanf("%f", &c);
+    if (c > 0){
+        printf("Formato incorrecto\n");
+        return(1);
+    }
+    if (c > MAX_TRANSACCION){
+        printf("Cantidad excede el limite establecido en este banco\n");
+        return(2);
+    }
+    printf("Iniciando ingreso\n");
+    user->saldo = user->saldo + c;
+    TRANSACCION transaccion;
+    time_t t;
+    transaccion.cantidad = c;
+    transaccion.ncuentas = user->ncuenta;
+    transaccion.ncuentao = NULL;
+    time(&t);
+    transaccion.fecha = localtime(&t);
+    ModificarCSV(user);
+    printf("Ingreso completado\n");
+    return(0);
 }
