7128721b2490ff2aac5b366237155b3827c1295e
Panch1027373 <jdpv645294509@gmail.com>
2025-04-03 18:15:43 +0200

    comentado escribirlogtrans

--- lab6/funciones/escribir_log_transacciones.c
+++ lab6/funciones/escribir_log_transacciones.c
@@ -3,13 +3,14 @@
 void EscribirLogTrans(TRANSACCION trans){
   char fecha[50], mensaje[100];
 
-  // Coje la fecha del sistema
+  // Coje la fecha de la transaccion
   strftime(fecha, sizeof(fecha), "%d-%m-%Y %H:%M:%S", trans.fecha);
   snprintf(mensaje, sizeof(mensaje),"%s %s %s %f %s\n", fecha, trans.ncuentas, trans.ncuentao, trans.cantidad, trans.descripcion);
-  // Bloquea el sistema por medio de semaforos
+  //SEM==========================================
   sem_t *sem = sem_open("/sem_log_trans", O_CREAT, 0644, 1);
-  FILE *log = fopen(Config.archivo_log_tranferencias,"a"); // Abre el fichero
-  fputs(trans.descripcion, log); // Añade la linea al final del fichero
+  FILE *log = fopen(Config.archivo_log_tranferencias,"a");
+  fputs(trans.descripcion, log);
   fclose(log);
   sem_post(sem);
+  //FIN SEM======================================
 }
