599639eae9473efaca6e98016d7b820c4639d7ab
Jaime <jocraha@gmail.com>
2025-04-11 09:56:53 +0200

    crear directorios para usuario y trans

--- practica-final/funciones/escribir_log_transacciones.c
+++ practica-final/funciones/escribir_log_transacciones.c
@@ -21,7 +21,14 @@ void EscribirLogTrans(TRANSACCION *trans){
   sem_t *semaforo = sem_open("/sem_log_trans", O_CREAT, 0644, 1);
   sem_wait(semaforo);
 
-  FILE *log_trans = fopen(Config.archivo_log_transferencias,"a");
+  // Crea el directorio de transacciones
+  char PathCuentaSalida[20];
+  snprintf(PathCuentaSalida, sizeof(PathCuentaSalida), "./transacciones/%s/transacciones.log", trans->ncuentas);
+
+  // Abre o crea y abre archivo log para guardar transaccion salida
+  FILE *log_trans = fopen(PathCuentaSalida,"a");
+
+  // En caso de fallo se resetean semaforos y se libera memoria
   if (!log_trans) {
     sem_post(semaforo);
     sem_close(semaforo);
@@ -31,9 +38,36 @@ void EscribirLogTrans(TRANSACCION *trans){
     return;
   }
 
+  FILE *log_trans2 = NULL;
+
+  // Si la transaccion tiene como objetivo a otro usuario, crea el log de la cuenta objetivo y escribe en el
+  if (trans->ncuentao != NULL) {
+    char PathCuentaObjetivo[20];
+    snprintf(PathCuentaObjetivo, sizeof(PathCuentaObjetivo), "./transacciones/%s/transacciones.log", trans->ncuentao);
+
+    // Abre o crea y abre archivo log para guardar transaccion objetivo
+    FILE *log_trans2 = fopen(PathCuentaObjetivo,"a");
+
+    // En caso de fallo se resetean semaforos y se libera memoria
+    if (!log_trans2) {
+      sem_post(semaforo);
+      sem_close(semaforo);
+      free(trans->ncuentas);
+      if (trans->ncuentao) free(trans->ncuentao);
+      free(trans);
+      return;
+    }
+  }
+
   fputs(mensaje, log_trans);
   fclose(log_trans);
 
+  if (trans->ncuentao != NULL)
+  {
+    fputs(mensaje, log_trans2);
+    fclose(log_trans2);
+  }
+  
   sem_post(semaforo);
   sem_close(semaforo);
   //FIN SEM======================================
--- practica-final/usuarios/crear_usuarios.c
+++ practica-final/usuarios/crear_usuarios.c
@@ -40,5 +40,12 @@ USER *crearUsuario(char *linea, int id){
     indice++;
   }
   user->id=id;
+
+  // Crea el directorio con el numero de la cuenta
+  char path[255];
+  snprintf(path, sizeof(path), "./transacciones/%d", user->ncuenta);
+  mkdrir(path, 0600); // 0[propietario][grupo usuario][otros usuarios]
+  // 600 = solo propietario puede escribir y leer 
+
   return user;
 }
